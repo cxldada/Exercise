ac813c9967c0cc8f8a1c23188e83a7fd5e37abcb
ChanXL <chan@ChanMacBook.local>
2020-01-11 23:22:35 +0800

    继续学习sds

--- copy_redis/src/sds.c
+++ copy_redis/src/sds.c
@@ -111,6 +111,7 @@ sds sdsnewlen(const void *init, size_t initlen) {
     return s;
 }
 
+// 创建一个空的长度为0的sds
 sds sdsempty(void) {
     return sdsnewlen("", 0);
 }
@@ -120,23 +121,135 @@ sds sdsnew(const char *init) {
     return sdsnewlen(init, initlen);
 }
 
+// 拷贝构造
 sds sdsdup(const sds s) {
     return sdsnewlen(s, sdslen(s));
 }
 
+// sds为空时，释放sds的空间
 void sdsfree(sds s) {
     if (s == NULL) return;
     s_free((char *)s - sdsHdrSize(s[-1]));
 }
 
+// 更新sds的长度
 void sdsupdatelen(sds s) {
     size_t reallen = strlen(s);
     sdssetlen(s, reallen);
 }
 
+// 清空sds的内容
 void sdsclear(sds s) {
     sdssetlen(s, 0);
     s[0] = '\0';
+}
+
+/************************* 提供给用户的低级API *************************/
+
+// 扩展sds的大小
+sds sdsMakeRoomFor(sds s, size_t addlen)
+{
+    void *sh, *newsh;
+    size_t avail = sdsavail(s);
+    size_t len = sdslen(s), newlen;
+    char type, oldtype = s[-1] & SDS_TYPE_MASK;
+    size_t hdrlen;
+
+    // 如果当前可用长度大于要添加的长度 可以直接使用
+    if (avail >= addlen)
+        return s;
+
+    // 计算newsize
+    hdrlen = sdsHdrSize(oldtype);
+    newlen = len + addlen;
+    if (newlen < SDS_MAX_PREALLOC)
+        newlen *= 2;
+    else
+        newlen += SDS_MAX_PREALLOC;
+
+    // 获取建议的长度
+    type = sdsReqType(newlen);
+    if (type == SDS_TYPE_5)
+        type = SDS_TYPE_8;
+
+    sh = (char *)s - hdrlen;
+    hdrlen = sdsHdrSize(type);
+    if (type == oldtype) {
+        newsh = s_realloc(sh, hdrlen + newlen);
+        if (newsh == NULL) return NULL;
+        s = (char *)newsh + hdrlen;
+    } else {
+        newsh = s_malloc(hdrlen + newlen + 1);
+        if (newsh == NULL) return NULL;
+        memcpy((char *)newsh + hdrlen, s, len + 1);
+        s_free(sh);
+        s = (char *)newsh + hdrlen;
+        s[-1] = type;
+        sdssetlen(s, len);
+    }
+    sdssetalloc(s, newlen);
+    return s;
+}
+
+// 删除sds多余的可用空间
+sds sdsRemoveFreeSpace(sds s) {
+    void *sh, *newsh;
+    char type, oldtype = s[-1] & SDS_TYPE_MASK;
+    int hdrlen, oldhdrlen = sdsHdrSize(oldtype);
+    size_t len = sdslen(s);
+    size_t avail = sdsavail(s);
+    sh = (char *)s - oldhdrlen;
+
+    // 如果没有可用空间了，直接返回
+    if (avail == 0) return s;
+
+    type = sdsReqType(len);
+    hdrlen = sdsHdrSize(type);
+
+    if (type == oldtype || type > SDS_TYPE_8) {
+        newsh = s_realloc(sh, len + 1);
+        if (newsh == NULL) return NULL;
+        s = (char *)newsh + hdrlen;
+    } else {
+        newsh = s_malloc(hdrlen + len + 1);
+        if (newsh == NULL) return NULL;
+        memcpy((char *)newsh + hdrlen, s, len + 1);
+        s_free(sh);
+        s = (char *)newsh + hdrlen;
+        s[-1] = type;
+        sdssetlen(s, len);
+    }
+
+    sdssetalloc(s, len);
+    return s;
+}
+
+/** 返回sds所使用的总内存
+ * 包括：
+ * 1 结构体大小
+ * 2 字符串大小
+ * 3 未使用的空间大小
+ * 4 结束符
+ * */
+size_t sdsAllocSize(sds s) {
+    size_t alloc = sdsalloc(s);
+    return (alloc + sdsHdrSize(s[1]) + 1);
+}
+
+/**
+ * 放回实际分配的指针
+ * */
+void *sdsAllocPtr(sds s) { return (void *)(s - sdsHdrSize(s[-1])); }
+
+/**
+ * 增加sds的长度并且删除结束符左侧多余的空间。通常会重新定位结束符的位置
+ * 
+ * 这个函数解决用户调用sdsMakeRoomFor()函数后字符串长度问题，在当前字符串后面添加一些内容，需要设置需的长度
+ * 
+ * 可以使用负增量
+ * */
+void sdsIncrLen(sds s, ssize_t incr) {
+
 }
 
 /*
@@ -164,7 +277,8 @@ void sdsrange(sds s, ssize_t start, ssize_t end);
 
 int sdscmp(const sds s1, const sds s2);
 
-sds *sdssplitlen(const char *s, ssize_t len, const char *sep, int seplen, int *count);
+sds *sdssplitlen(const char *s, ssize_t len, const char *sep, int seplen, int
+*count);
 
 void sdsgreesplitres(sds *tokens, int count);
 
@@ -184,14 +298,4 @@ sds sdsjoin(char **argv, int argc, char *sep);
 
 sds sdsjoinsds(sds *argv, int argc, const char *sep, size_t seplen);
 
-// 提供给用户的低级API
-sds sdsMakeRoomFor(sds s, size_t addlen);
-
-void sdsIncrLen(sds s, ssize_t incr);
-
-sds sdsRemoveFreeSpace(sds s);
-
-size_t sdsAllocSize(sds s);
-
-void *sdsAllocPtr(sds s);
 */
