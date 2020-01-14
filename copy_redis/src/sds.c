9b8be8463b857bc94f0cc391d629f5539b175e02
ChanXL <chan@ChanMacBook.local>
2020-01-14 22:01:32 +0800

    修改bug

--- copy_redis/src/sds.c
+++ copy_redis/src/sds.c
@@ -852,14 +852,14 @@ sds sdsjoin(char **argv, int argc, char *sep) {
         if (j != argc - 1) join = sdscat(join, sep);
     }
 
-    return s;
+    return join;
 }
 
 sds sdsjoinsds(sds *argv, int argc, const char *sep, size_t seplen) {
     sds join = sdsempty();
     int j;
 
-    for (j = 0; j < argc, j++) {
+    for (j = 0; j < argc; j++) {
         join = sdscatsds(join, argv[j]);
         if (j != argc - 1) join = sdscatlen(join, sep, seplen);
     }
