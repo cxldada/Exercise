f439f7e0cd5b45f885ffd44dcf303c9d6b17e448
unknown <ChenXiaoLong@shirojoy.org>
2020-03-16 19:42:12 +0800

    modify something

--- playground/test.cpp
+++ playground/test.cpp
@@ -1,11 +1,35 @@
-#include <iostream>
 #include <math.h>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <ctime>
+
 using namespace std;
 
-int main(int argc,char *argv[]){
+int main(int argc, char *argv[]) {
+    /*
     double a,b;
     a = 10.1;
     b = ceil(a);
     cout << a << "  " << b << endl;
+
+    unsigned min, max;
+    size_t tokenPos;
+    string str = "12|13", maxs, mins;
+
+    if ((tokenPos = str.find("|")) == std::string::npos) {
+        return 1;
+    }
+
+    mins = str.substr(0, tokenPos);
+    maxs = str.substr(tokenPos + 1);
+
+    // min = atoi(mins.c_str());
+    max = atoi("111");
+    cout << tokenPos << " " << min << " " << max << endl;
+    */
+
+    cout << sizeof(time_t) << endl;
+
     return 0;
 }
