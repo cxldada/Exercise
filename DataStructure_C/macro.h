032cd0eabb7d151bed298e8e8884c0617f747a31
cxldada <c.x.l@live.com>
2020-05-06 17:34:50 +0800

    category markdown

--- DataStructure_C/macro.h
+++ DataStructure_C/macro.h
@@ -1,16 +1,14 @@
 #ifndef __MACRO_H__
 #define __MACRO_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-
 #define OK 1
 #define ERROR 0
-#define OVERFLOW -1
-#define Status int
+#define INFEASIBLE -1
+#define OVERFLOW -2
 #define TRUE 1
 #define FALSE 0 
 #define _MAXSIZE_ 100
+
+typedef int Status;
 
 #endif
