3395b64ee22f59d2270013baa0473953637380c3
cxldada <c.x.l@live.com>
2020-05-21 17:14:16 +0800

    KMP algorithm

--- DataStructure_C/KMP_Algorithm.c
+++ DataStructure_C/KMP_Algorithm.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// a b a a b c
-// 0 1 
 void getPrefixTable(char *temp, int len, int *ret) {
-    int l = 0, j = 0;
-    ret[0] = 0;
-    while (l < len) {
-        if (j == 0 || temp[l] == temp[j]) {
-            ++l;
+    int i = 0, j = -1;
+    ret[0] = -1;
+    while (i < len) {
+        if (j == -1 || temp[i] == temp[j]) {
+            ++i;
             ++j;
-            ret[l] = j;
+            if (i < len)
+                ret[i] = j;
         } else {
             j = ret[j];
         }
     }
 }
 
-int index(char *str, int slen, char *temp, int tlen) {
+int search_index(char *str, char *temp, int pos) {
+    int i = pos, j = 0;
+    int slen = strlen(str);
+    int tlen = strlen(temp);
+    int next[tlen];
+    getPrefixTable(temp, tlen, next);
+    while (i < slen && j < tlen) {
+        if (j == -1 || str[i] == temp[j]) {
+            ++i;
+            ++j;
+        } else {
+            j = next[j];
+        }
+    }
+    if (j >= tlen)
+        return i - tlen;
+    else return 0;
 }
 
 int main(int argc, char const *argv[])
 {
+    char *str = "BBC ABCDAB ABCDABCDABDE";
+    char *temp = "ABCDABD";
+    int index = search_index(str, temp, 10);
+    printf("index: %d", index);
+
     return 0;
 }
 
