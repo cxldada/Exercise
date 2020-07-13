93836c725e94e8586bd448056ded56d075805769
cxldada <c.x.l@live.com>
2020-07-13 15:18:18 +0800

    测试dup后是否为统一文件表项

--- c/dup_dup2.c
+++ c/dup_dup2.c
@@ -6,12 +6,15 @@
 /***
  * 系统默认是关闭 FD_CLOEXEC 标志的
  * dup函数会将新文件描述符的标志清除掉，这里测试一下
+ * 
+ * 测试dup后是否为同一个文件表项
  * */
 
 int main(int argc, char const *argv[])
 {
     int fd, dfd, d2fd;
     int declare;
+    off_t start,end;
     // create file
     fd = open("./newfile", O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, S_IRWXO);
     if(fd == -1) {
@@ -34,6 +37,10 @@ int main(int argc, char const *argv[])
     declare = fcntl(fd, F_GETFD);
     printf("after change fd: %d\n", declare);
 
+    // get offset
+    start = lseek(fd, 0, SEEK_CUR);
+    printf("fd offset: %lld\n", start);
+
     // dup
     dfd = dup(fd);
 
@@ -41,12 +48,20 @@ int main(int argc, char const *argv[])
     declare = fcntl(dfd, F_GETFD, declare);
     printf("dup fd: %d\n", declare);
 
+    // get offset
+    end = lseek(dfd, 0, SEEK_CUR);
+    printf("dup fd offset: %lld\n", end);
+
     // dup2
     d2fd = dup2(fd, 10);
 
     // check file declare
     declare = fcntl(d2fd, F_GETFD, declare);
     printf("dup2 fd: %d\n", declare);
+
+    // get offset
+    end = lseek(d2fd, 0, SEEK_CUR);
+    printf("dup2 fd offset: %lld\n", end);
 
 
     exit(0);
