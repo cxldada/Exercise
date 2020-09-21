779f3570241a2cbb57c38b2e6ac906ebd73a92e8
cxldada <c.x.l@live.com>
2020-09-21 21:34:56 +0800

    顺序表的实现

---
+++ DataStructure_C/standard/Sqlist.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Sqlist.h"
+
+// 构造一个空的顺序表
+Status InitList_Sq(Sqlist *l){
+    l->elem = (ElemType *)malloc(LIST_INIT_SIZE * sizeof(ElemType));
+    if(!l->elem)
+        exit(OVERFLOW);
+    l->length = 0;
+    l->listsize = LIST_INIT_SIZE;
+    return OK;
+}
+
+// 销毁一个已存在的顺序表
+Status DestoryList_Sq(Sqlist *l) {
+    free(l->elem);
+    l->length = 0;
+    l->listsize = 0;
+    return OK;
+}
+
+// 清空顺序表的元素
+Status ClearList_Sq(Sqlist *l) {
+    l->length = 0;
+    return OK;
+}
+
+// 判断顺序表是否为空
+Status ListEmpty_Sq(Sqlist l) {
+    return (l.length == 0);
+}
+
+// 获取顺序表的元素个数
+Status ListLength_Sq(Sqlist l) {
+    return l.length;
+}
+
+// 获取元素
+Status GetElem_Sq(Sqlist l, int i, ElemType *e) {
+    if(l.length < i) {
+        return OVERFLOW;
+    }
+
+    *e = l.elem[i];
+    return OK;
+}
+
+// 在第i个位置之前插入新的元素e
+Status ListInsert_Sq(Sqlist *l, int i, ElemType e) {
+    if (i < 1 || i > (l->listsize + 1))
+        return ERROR;
+    
+    if(l->length >= l->listsize) { // 增加空间
+        ElemType *newbase;
+        newbase = (ElemType *)realloc(l->elem, ((l->length + LIST_INCREMENT) * sizeof(ElemType)));
+        if(!newbase)
+            exit(OVERFLOW);
+
+        l->elem = newbase;
+        l->listsize += LIST_INCREMENT;
+    }
+
+    for (int p = l->length; p >= i; --p) {
+        l->elem[p] = l->elem[p - 1];
+    }
+    l->elem[i - 1] = e;
+    ++(l->length);
+    return OK;
+}
+
+// 删除第i个位置上的元素，并用e返回其值
+Status ListDelete_Sq(Sqlist *l, int i, ElemType *e) {
+    if(l->length == 0 || i < 1 || i > l->length)
+        return ERROR;
+
+    *e = l->elem[(i - 1)];
+    for (int p = i; p < l->listsize; ++p) {
+        l->elem[(i - 1)] = l->elem[i];
+    }
+    --(l->listsize);
+    return OK;
+}
+
+// 查找第一个与e符合compare关系的元素
+// int compare(ElemType a, ElemType b){}
+int LocateElem_Sq(Sqlist l, ElemType e, int *compare(ElemType, ElemType)) {
+    for (int i = 0; i < l.length; ++i) {
+        if (compare(l.elem[i], e) == 0) {
+            return (i + 1);
+        }
+    }
+
+    return 0;
+}
+
+// 遍历打印顺序表
+Status ListTraverse(Sqlist l) {
+    printf("顺序表中的元素有: ");
+    for (int i = 0; i < l.length; ++i) {
+        printf("%d ", l.elem[i]);
+    }
+    printf("\n");
+    return OK;
+}
+
+// 合并两个递增顺序
+void MergeList_Sq(Sqlist la, Sqlist lb, Sqlist *lc) {
+    int len_a, len_b, i, j, k;
+
+    len_a = la.length;
+    len_b = lb.length;
+
+    lc->elem = (ElemType *)malloc((len_a + len_b) * sizeof(ElemType));
+    if(!lc->elem)
+        exit(OVERFLOW);
+    lc->length = 0;
+    lc->listsize = len_a + len_b;
+
+    i = 0;
+    j = 0;
+    k = 0;
+    while (i < la.length && j < lb.length) {
+        if(la.elem[i] < lb.elem[j]) {
+            lc->elem[k++] = la.elem[i++];
+        } else {
+            lc->elem[k++] = lb.elem[j++];
+        }
+    }
+
+    while(i < la.length) {
+        lc->elem[k++] = la.elem[i++];
+    }
+    while(j < lb.length) {
+        lc->elem[k++] = lb.elem[j++];
+    }
+}
---
+++ DataStructure_C/standard/Sqlist.h
@@ -0,0 +1,62 @@
+#ifndef _SEQUENCE_LIST_H_
+#define _SEQUENCE_LIST_H_
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+// 线性表的存储空间的初始分配量
+#define LIST_INIT_SIZE 100
+
+// 线性表存储空间的分配增量
+#define LIST_INCREMENT 10
+
+typedef int Status;
+typedef int ElemType;
+
+typedef struct {
+    ElemType *elem; // 存储空间基址
+    int length; // 当前长度
+    int listsize; // 当前分配的存储容量(以sizeof(ElemType))为单位
+} Sqlist;
+
+/******************** 操作声明 **************************/
+
+// 构造一个空的顺序表
+Status InitList_Sq(Sqlist *l);
+
+// 销毁一个已存在的顺序表
+Status DestoryList_Sq(Sqlist *l);
+
+// 清空顺序表的元素
+Status ClearList_Sq(Sqlist *l);
+
+// 判断顺序表是否为空
+Status ListEmpty_Sq(Sqlist l);
+
+// 获取顺序表的元素个数
+Status ListLength_Sq(Sqlist l);
+
+// 获取元素
+Status GetElem_Sq(Sqlist l, int i, ElemType *e);
+
+// 在第i个位置之前插入新的元素e
+Status ListInsert_Sq(Sqlist *l, int i, ElemType e);
+
+// 删除第i个位置上的元素，并用e返回其值
+Status ListDelete_Sq(Sqlist *l, int i, ElemType *e);
+
+// 查找第一个与e符合compare关系的元素
+// int compare(ElemType a, ElemType b);
+int LocateElem_Sq(Sqlist l, ElemType e, int *compare(ElemType, ElemType));
+
+// 遍历打印顺序表
+Status ListTraverse(Sqlist l);
+
+// 合并两个递增顺序
+void MergeList_Sq(Sqlist la, Sqlist lb, Sqlist *lc);
+
+#endif
